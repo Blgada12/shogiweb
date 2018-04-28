4079202a5a995d6d2bd1df221462f62cfe16e8f0
김도현 <gimdohyeon@gimdohyeon-ui-MacBook-Pro.local>
2018-04-28 21:02:36 +0900

    pieceMove

--- src/components/shogi.vue
+++ src/components/shogi.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
     <h1>쇼기 테스트</h1>
-    <table id="board">
-      <tr v-for="i in Array.from(Array(9).keys()) " v-bind:key="i">
-        <td v-for="j in Array.from(Array(9).keys())" v-bind:key="j">
+    <table id="enemySaves" class="saves">
+      <tr>
+        <td v-for="i in Array.from(Array(enemySave.length).keys())" v-bind:key="i">
+          <div v-bind:id="'m-'+'i'" class="enemyPiece">
+            {{ enemySave[i].display }}
+          </div>
+        </td>
+      </tr>
+    </table>
+    <table id="board" v-on:click="boardClick">
+      <tr v-for="i in Array.from(Array(nowBoard.length).keys()) " v-bind:key="i">
+        <td v-for="j in Array.from(Array(nowBoard[0].length).keys())" v-bind:key="j">
           <div v-bind:id="i+'-'+j" v-bind:class="nowBoard[i][j].isMy">
             {{ nowBoard[i][j].display }}
+          </div>
+        </td>
+      </tr>
+    </table>
+    <table id="mySaves" class="saves">
+      <tr>
+        <td v-for="i in Array.from(Array(mySave.length).keys())" v-bind:key="i">
+          <div v-bind:id="'m-'+'i'" class="myPiece">
+            {{ mySave[i].display }}
           </div>
         </td>
       </tr>
@@ -27,63 +45,54 @@ class Piece {
 class Blank {
   display = ''
 }
-
 class Gokusho extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '玉'
   }
 }
-
 class Hisha extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '飛'
   }
 }
-
 class Kakukyo extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '角'
   }
 }
-
 class Kinsho extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '金'
   }
 }
-
 class Ginsho extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '銀'
   }
 }
-
 class Keima extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '桂'
   }
 }
-
 class Kyosha extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '香'
   }
 }
-
 class Huhyo extends Piece {
   constructor (isMy) {
     super(isMy)
     this.display = '歩'
   }
 }
-
 const firstBoard = [
   [
     new Kyosha(false), new Keima(false), new Ginsho(false), new Kinsho(false), new Gokusho(false), new Kinsho(false), new Ginsho(false), new Keima(false), new Kyosha(false)
@@ -112,37 +121,47 @@ const firstBoard = [
   [
     new Kyosha(true), new Keima(true), new Ginsho(true), new Kinsho(true), new Gokusho(true), new Kinsho(true), new Ginsho(true), new Keima(true), new Kyosha(true)
   ]
-
 ]
 
-let nowDisplay = []
-
-nowDisplay = firstBoard
-
-const downPiece = (p0, p1) => {
-  if (nowDisplay[p1[0]][p1[1]].display === '') {
-    console.log('비어있는고임~')
-  } else {
-    console.log('뭔가 있는고임')
-  }
-}
-
-window.onload = () => {
-  const board = document.getElementById('board')
-
-  board.addEventListener('click', (e) => {
-    if (e.target.nodeName === 'DIV') {
-      console.log(e.target.id.split('-'))
-      downPiece(nowDisplay[8][8], e.target.id.split('-'))
-    }
-  })
-}
+let pieceSelect = false
+let pieceSelectPlace = []
 
 export default {
   name: 'shogi',
   data: function () {
     return {
-      nowBoard: nowDisplay
+      nowBoard: firstBoard,
+      mySave: [],
+      enemySave: []
+    }
+  },
+  methods: {
+    downPiece: function (p0, p1) {
+      if (this.nowBoard[p1[0]][p1[1]].display === '') {
+        this.nowBoard[p1[0]].splice(p1[1], 1, this.nowBoard[p0[0]][p0[1]])
+        this.nowBoard[p0[0]].splice(p0[1], 1, new Blank())
+      } else {
+        this.mySave.push(this.nowBoard[p1[0]][p1[1]])
+        this.nowBoard[p1[0]].splice(p1[1], 1, this.nowBoard[p0[0]][p0[1]])
+        this.nowBoard[p0[0]].splice(p0[1], 1, new Blank())
+        console.log(this.mySave)
+      }
+    },
+    boardClick: function (e) {
+      console.log(e.target)
+      if (e.target.nodeName === 'DIV') {
+        if (pieceSelect) {
+          if (e.target.className !== 'myPiece') {
+            this.downPiece(pieceSelectPlace, e.target.id.split('-'))
+            pieceSelect = false
+          }
+        } else {
+          if (e.target.className === 'myPiece') {
+            pieceSelectPlace = e.target.id.split('-')
+            pieceSelect = true
+          }
+        }
+      }
     }
   }
 }
@@ -159,12 +178,14 @@ export default {
 #board>tr>td{
   border: 1px solid;
 }
-#board>tr>td>div{
+table>tr>td>div{
   display:table-cell;
   text-align: center;
   vertical-align:middle;
   width: 50px;
   height: 50px;
+  margin: 0px;
+  padding: 0px;
 }
 .myPiece{
   background: url('../assets/images/piece.png') no-repeat center center;
@@ -176,5 +197,8 @@ export default {
   background-size: 40px;
   font-size: 20px;
   transform: rotate(180deg);
+}
+.saves{
+  height: 60px;
 }
 </style>
