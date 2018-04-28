8a67add5a63511c8a6277875b8b1c487ebf06821
김도현 <gimdohyeon@gimdohyeon-ui-MacBook-Pro.local>
2018-04-28 14:59:09 +0900

    Graphic upgrade, click

--- src/components/shogi.vue
+++ src/components/shogi.vue
@@ -2,10 +2,10 @@
   <div>
     <h1>쇼기 테스트</h1>
     <table id="board">
-      <tr v-for="i in nowBoard" v-bind:key="i">
-        <td v-for="j in i" v-bind:key="j">
-          <div draggable="true" v-bind:id="'b'+j.position[0]+j.position[1]" v-bind:class="j.isMy ? 'myPiece' : 'enemyPiece'">
-            {{ j.display }}
+      <tr v-for="i in Array.from(Array(9).keys()) " v-bind:key="i">
+        <td v-for="j in Array.from(Array(9).keys())" v-bind:key="j">
+          <div v-bind:id="i+'-'+j" v-bind:class="nowBoard[i][j].isMy">
+            {{ nowBoard[i][j].display }}
           </div>
         </td>
       </tr>
@@ -16,131 +16,101 @@
 <script>
 
 class Piece {
-  constructor (position, isMy) {
-    this.position = position
-    this.isMy = isMy
-  }
-  pieceUp () {
-
+  constructor (isMy) {
+    if (isMy) {
+      this.isMy = 'myPiece'
+    } else {
+      this.isMy = 'enemyPiece'
+    }
   }
 }
 class Blank {
-  constructor (position) {
-    this.position = position
-    this.display = ''
-  }
-  pieceDown (position) {
-    return false
-  }
+  display = ''
 }
 
 class Gokusho extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '玉'
-  }
-  move (position) {
-    return false
   }
 }
 
 class Hisha extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '飛'
-  }
-  move (position) {
-    return false
   }
 }
 
 class Kakukyo extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '角'
-  }
-  move (position) {
-    return false
   }
 }
 
 class Kinsho extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '金'
-  }
-  move (position) {
-    return false
   }
 }
 
 class Ginsho extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '銀'
-  }
-  move (position) {
-    return false
   }
 }
 
 class Keima extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '桂'
-  }
-  move (position) {
-    return false
   }
 }
 
 class Kyosha extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '香'
-  }
-  move (position) {
-    return false
   }
 }
 
 class Huhyo extends Piece {
-  constructor (position, isMy) {
-    super(position, isMy)
+  constructor (isMy) {
+    super(isMy)
     this.display = '歩'
-  }
-  move (position) {
-    return false
   }
 }
 
 const firstBoard = [
   [
-    new Kyosha([0, 0], false), new Keima([0, 1], false), new Ginsho([0, 2], false), new Kinsho([0, 3], false), new Gokusho([0, 4], false), new Kinsho([0, 5], false), new Ginsho([0, 6], false), new Keima([0, 7], false), new Kyosha([0, 8], false)
+    new Kyosha(false), new Keima(false), new Ginsho(false), new Kinsho(false), new Gokusho(false), new Kinsho(false), new Ginsho(false), new Keima(false), new Kyosha(false)
   ],
   [
-    new Blank([1, 0]), new Hisha([1, 1], false), new Blank([1, 2]), new Blank([1, 3]), new Blank([1, 4]), new Blank([1, 5]), new Blank([1, 6]), new Kakukyo([1, 7], false), new Blank([1, 8])
+    new Blank(), new Hisha(false), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Kakukyo(false), new Blank()
   ],
   [
-    new Huhyo([2, 0], false), new Huhyo([2, 1], false), new Huhyo([2, 2], false), new Huhyo([2, 3], false), new Huhyo([2, 4], false), new Huhyo([2, 5], false), new Huhyo([2, 6], false), new Huhyo([2, 7], false), new Huhyo([2, 8], false)
+    new Huhyo(false), new Huhyo(false), new Huhyo(false), new Huhyo(false), new Huhyo(false), new Huhyo(false), new Huhyo(false), new Huhyo(false), new Huhyo(false)
   ],
   [
-    new Blank([3, 0]), new Blank([3, 1]), new Blank([3, 2]), new Blank([3, 3]), new Blank([3, 4]), new Blank([3, 5]), new Blank([3, 6]), new Blank([3, 7]), new Blank([3, 8])
+    new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank()
   ],
   [
-    new Blank([4, 0]), new Blank([4, 1]), new Blank([4, 2]), new Blank([4, 3]), new Blank([4, 4]), new Blank([4, 5]), new Blank([4, 6]), new Blank([4, 7]), new Blank([4, 8])
+    new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank()
   ],
   [
-    new Blank([5, 0]), new Blank([5, 1]), new Blank([5, 2]), new Blank([5, 3]), new Blank([5, 4]), new Blank([5, 5]), new Blank([5, 6]), new Blank([5, 7]), new Blank([5, 8])
+    new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Blank()
   ],
   [
-    new Huhyo([6, 0], true), new Huhyo([6, 1], true), new Huhyo([6, 2], true), new Huhyo([6, 3], true), new Huhyo([6, 4], true), new Huhyo([6, 5], true), new Huhyo([6, 6], true), new Huhyo([6, 7], true), new Huhyo([6, 8], true)
+    new Huhyo(true), new Huhyo(true), new Huhyo(true), new Huhyo(true), new Huhyo(true), new Huhyo(true), new Huhyo(true), new Huhyo(true), new Huhyo(true)
   ],
   [
-    new Blank([7, 0]), new Kakukyo([7, 1], true), new Blank([7, 2]), new Blank([7, 3]), new Blank([7, 4]), new Blank([7, 5]), new Blank([7, 6]), new Hisha([7, 7], true), new Blank([7, 8])
+    new Blank(), new Kakukyo(true), new Blank(), new Blank(), new Blank(), new Blank(), new Blank(), new Hisha(true), new Blank()
   ],
   [
-    new Kyosha([8, 0], true), new Keima([8, 1], true), new Ginsho([8, 2], true), new Kinsho([8, 3], true), new Gokusho([8, 4], true), new Kinsho([8, 5], true), new Ginsho([8, 6], true), new Keima([8, 7], true), new Kyosha([8, 8], true)
+    new Kyosha(true), new Keima(true), new Ginsho(true), new Kinsho(true), new Gokusho(true), new Kinsho(true), new Ginsho(true), new Keima(true), new Kyosha(true)
   ]
 
 ]
@@ -149,14 +119,29 @@ let nowDisplay = []
 
 nowDisplay = firstBoard
 
-console.log(nowDisplay)
+const downPiece = (p0, p1) => {
+  if (nowDisplay[p1[0]][p1[1]].display === '') {
+    console.log('비어있는고임~')
+  } else {
+    console.log('뭔가 있는고임')
+  }
+}
 
+window.onload = () => {
+  const board = document.getElementById('board')
+
+  board.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'DIV') {
+      console.log(e.target.id.split('-'))
+      downPiece(nowDisplay[8][8], e.target.id.split('-'))
+    }
+  })
+}
 
 export default {
   name: 'shogi',
   data: function () {
     return {
-      msg: 'Welcome to Shogi Web',
       nowBoard: nowDisplay
     }
   }
@@ -172,16 +157,23 @@ export default {
   border: 1px solid;
 }
 #board>tr>td{
-  text-align: center;
-  width: 50px;
-  height: 50px;
   border: 1px solid;
 }
+#board>tr>td>div{
+  display:table-cell;
+  text-align: center;
+  vertical-align:middle;
+  width: 50px;
+  height: 50px;
+}
 .myPiece{
+  background: url('../assets/images/piece.png') no-repeat center center;
+  background-size: 40px;
   font-size: 20px;
 }
 .enemyPiece{
-  color: red;
+  background: url('../assets/images/piece.png') no-repeat center center;
+  background-size: 40px;
   font-size: 20px;
   transform: rotate(180deg);
 }
